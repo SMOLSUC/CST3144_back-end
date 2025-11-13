f5a1353435b13391138cb1ccf3cb1ed2301a7cf7
SMOLSUC <fokxys@gmail.com>
2025-11-13 16:13:42 +0000

    remove the second server

--- server.js
+++ server.js
@@ -91,6 +91,5 @@ app.put("/api/lessons/:id/decrement", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
